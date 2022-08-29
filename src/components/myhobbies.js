779aebdb8c77eb1feb456916920af791b29bf3f1
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-08-29 18:51:10 -0500

    basic optimization for all devices done

--- src/components/myhobbies.js
+++ src/components/myhobbies.js
@@ -3,16 +3,7 @@ import './myhobbies.css';
 
 const Hobbies = () => {
   return(
-    //<p>My hobbies will be listed here</p>
-    <div id="pillar">
-
-<div class="row">
-  <div class="wrap">
-    <div class="left">Art is pointless</div>
-    <div class="right">have fun pronouncing this</div>  
-    </div>
-</div>
-</div>
+    <div>My hobbies will be posted later!</div>
   );
 }
 
--- src/components/projects.js
+++ src/components/projects.js
@@ -3,15 +3,7 @@ import React from 'react';
 
 const Projects = () => {
   return(
-    // <p>hhhh</p>
-    <div class="ticker-wrap">
-      <div class="ticker">
-        <div class="ticker__item">Letterpress chambray brunch.</div>
-        <div class="ticker__item">Vice mlkshk crucifix beard chillwave meditation hoodie asymmetrical Helvetica.</div>
-        <div class="ticker__item">Ugh PBRB kale chips Echo Park.</div>
-        <div class="ticker__item">Gluten-free mumblecore chambray mixtape food truck. </div>
-      </div>
-    </div>
+    <p>Projects will be posted later!</p>
   );
 }
 
