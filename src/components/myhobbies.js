9e135cd8616b7fadff09e0cee346c1b35ae2c314
Tim Goloshchapov <tim.goloschapov@gmail.com>
2023-04-13 01:10:57 -0500

    phone and pc version wip

--- src/components/myhobbies.js
+++ src/components/myhobbies.js
@@ -6,8 +6,8 @@ const Hobbies = () => {
     <div className='hobbiesPage'>
       <span className='hobbiesTitle'>My Hobbies</span>
       <div class="wrapper">
-        <img src="https://source.unsplash.com/random/600x600?water" alt=""/>
-        <img src="https://source.unsplash.com/random/600x600?summer" alt=""/>
+        <img className='imgOne' src="https://source.unsplash.com/random/600x600?water" alt=""/>
+        <img className='imgTwo' src="https://source.unsplash.com/random/600x600?summer" alt=""/>
         <img src="https://source.unsplash.com/random/600x600?plants" alt=""/>
         <img src="https://source.unsplash.com/random/600x600?snow" alt=""/>
         <img src="https://source.unsplash.com/random/600x600?roses" alt=""/>
@@ -15,7 +15,7 @@ const Hobbies = () => {
         <img src="https://source.unsplash.com/random/600x600?nature" alt=""/>
         <img src="https://source.unsplash.com/random/600x600?blossom" alt=""/>
         <img src="https://source.unsplash.com/random/600x600?ice" alt=""/>
-        <img src="https://source.unsplash.com/random/600x600?spring" alt=""/> 
+        <img className='imgLast' src="https://source.unsplash.com/random/600x600?spring" alt=""/> 
       </div>
     </div>
   );
