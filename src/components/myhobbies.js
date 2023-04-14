5eb07a4eb3632a7f5dab8f0c9b0de866ea4c1be6
Tim Goloshchapov <tim.goloschapov@gmail.com>
2023-04-14 00:24:58 -0500

    text on hover wip

--- src/components/myhobbies.js
+++ src/components/myhobbies.js
@@ -2,14 +2,24 @@ import React from 'react';
 import './myhobbies.css';
 
 const Hobbies = () => {
+
+  function showDescription() {
+    var x = document.getElementById("text");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
   return(
     <div className='hobbiesPage'>
       <span className='hobbiesTitle'>My Hobbies</span>
       <div class="wrapper">
         <img className='imgOne' src="https://source.unsplash.com/random/600x600?water" alt=""/>
         <img className='imgTwo' src="https://source.unsplash.com/random/600x600?summer" alt=""/>
-        <img src="https://source.unsplash.com/random/600x600?plants" alt=""/>
-        <img src="https://source.unsplash.com/random/600x600?snow" alt=""/>
+        <img src="https://source.unsplash.com/random/600x600?plants" onMouseEnter={showDescription} onMouseLeave={showDescription} alt=""/>
+        <img src="https://source.unsplash.com/random/600x600?snow" onMouseEnter={showDescription} onMouseLeave={showDescription} alt=""/>
         <img src="https://source.unsplash.com/random/600x600?roses" alt=""/>
         <img src="https://source.unsplash.com/random/600x600?sky" alt=""/>
         <img src="https://source.unsplash.com/random/600x600?nature" alt=""/>
@@ -17,6 +27,7 @@ const Hobbies = () => {
         <img src="https://source.unsplash.com/random/600x600?ice" alt=""/>
         <img className='imgLast' src="https://source.unsplash.com/random/600x600?spring" alt=""/> 
       </div>
+      <span id='text'>I like water a lot!!!!</span>
     </div>
   );
 }
