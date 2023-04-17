fc59747771fb75e87dca5803eae8f1bcb007e041
Tim Goloshchapov <tim.goloschapov@gmail.com>
2023-04-16 22:04:29 -0500

    images with text wip

--- src/components/myhobbies.js
+++ src/components/myhobbies.js
@@ -3,34 +3,73 @@ import './myhobbies.css';
 
 const Hobbies = () => {
 
-  function showDescription() {
-    var x = document.getElementById("text");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
 
   return(
     <div className='hobbiesPage'>
       <span className='hobbiesTitle'>My Hobbies</span>
       <div class="wrapper">
-        <img className='imgOne' src="https://source.unsplash.com/random/600x600?water" alt=""/>
-        <img className='imgTwo' src="https://source.unsplash.com/random/600x600?summer" alt=""/>
-        <img src="https://source.unsplash.com/random/600x600?plants" onMouseEnter={showDescription} onMouseLeave={showDescription} alt=""/>
-        <img src="https://source.unsplash.com/random/600x600?snow" onMouseEnter={showDescription} onMouseLeave={showDescription} alt=""/>
-        <img src="https://source.unsplash.com/random/600x600?roses" alt=""/>
-        <img src="https://source.unsplash.com/random/600x600?sky" alt=""/>
-        <img src="https://source.unsplash.com/random/600x600?nature" alt=""/>
-        <img src="https://source.unsplash.com/random/600x600?blossom" alt=""/>
-        <img src="https://source.unsplash.com/random/600x600?ice" alt=""/>
-        <img className='imgLast' src="https://source.unsplash.com/random/600x600?spring" alt=""/>
-      </div>
-      <div className='textDiv'>
-        <span id='text'>I like water a lot!!!!</span> 
+        <div class="image">
+          <img src="https://source.unsplash.com/random/600x600?water" alt=""/>
+          <div class="description">
+            <p>I like water a lot!!!!</p>
+          </div>
+        </div>
+        <div class="image">
+          <img src="https://source.unsplash.com/random/600x600?summer" alt=""/>
+          <div class="description">
+            <p>summer is dope</p>
+          </div>
+        </div>
+        <div class="image">
+          <img src="https://source.unsplash.com/random/600x600?plants" alt=""/>
+          <div class="description">
+            <p>plantssssss</p>
+          </div>
+        </div>
+        <div class="image">
+          <img src="https://source.unsplash.com/random/600x600?snow" alt=""/>
+          <div class="description">
+            <p>snowyyyyy</p>
+          </div>
+        </div>
+        <div class="image">
+          <img src="https://source.unsplash.com/random/600x600?roses" alt=""/>
+          <div class="description">
+            <p>roses are red</p>
+          </div>
+        </div>
+        <div class="image">
+          <img src="https://source.unsplash.com/random/600x600?sky" alt=""/>
+          <div class="description">
+            <p>sky is meh</p>
+          </div>
+        </div>
+        <div class="image">
+          <img src="https://source.unsplash.com/random/600x600?nature" alt=""/>
+          <div class="description">
+            <p>nature is natural</p>
+          </div>
+        </div>
+        <div class="image">
+          <img src="https://source.unsplash.com/random/600x600?blossom" alt=""/>
+          <div class="description">
+            <p>blossomy blossom</p>
+          </div>
+        </div>
+        <div class="image">
+          <img src="https://source.unsplash.com/random/600x600?ice" alt=""/>
+          <div class="description">
+            <p>ice babyyyyy</p>
+          </div>
+        </div>
+        <div class="image">
+          <img src="https://source.unsplash.com/random/600x600?spring" alt=""/>
+          <div class="description">
+            <p>spring is allergic</p>
+          </div>
       </div>
     </div>
+  </div>
   );
 }
 
