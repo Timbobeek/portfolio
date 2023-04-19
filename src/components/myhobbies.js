ef9f04ea55ef293e2d9230dd82578cef0800170c
Tim Goloshchapov <tim.goloschapov@gmail.com>
2023-04-18 22:01:18 -0500

    overall margins defined

--- src/components/myhobbies.js
+++ src/components/myhobbies.js
@@ -7,64 +7,64 @@ const Hobbies = () => {
   return(
     <div className='hobbiesPage'>
       <span className='hobbiesTitle'>My Hobbies</span>
-      <div class="wrapper">
-        <div class="image">
+      <div className="wrapper">
+        <div className="image">
           <img src="https://source.unsplash.com/random/600x600?water" alt=""/>
-          <div class="description">
+          <div className="description">
             <p>I like water a lot!!!!</p>
           </div>
         </div>
-        <div class="image">
+        <div className="image" id='img2'>
           <img src="https://source.unsplash.com/random/600x600?summer" alt=""/>
-          <div class="description">
+          <div className="description">
             <p>summer is dope</p>
           </div>
         </div>
-        <div class="image">
+        <div className="image">
           <img src="https://source.unsplash.com/random/600x600?plants" alt=""/>
-          <div class="description">
+          <div className="description">
             <p>plantssssss</p>
           </div>
         </div>
-        <div class="image">
+        <div className="image">
           <img src="https://source.unsplash.com/random/600x600?snow" alt=""/>
-          <div class="description">
+          <div className="description">
             <p>snowyyyyy</p>
           </div>
         </div>
-        <div class="image">
+        <div className="image">
           <img src="https://source.unsplash.com/random/600x600?roses" alt=""/>
-          <div class="description">
+          <div className="description">
             <p>roses are red</p>
           </div>
         </div>
-        <div class="image">
+        <div className="image">
           <img src="https://source.unsplash.com/random/600x600?sky" alt=""/>
-          <div class="description">
+          <div className="description">
             <p>sky is meh</p>
           </div>
         </div>
-        <div class="image">
+        <div className="image">
           <img src="https://source.unsplash.com/random/600x600?nature" alt=""/>
-          <div class="description">
+          <div className="description">
             <p>nature is natural</p>
           </div>
         </div>
-        <div class="image">
+        <div className="image">
           <img src="https://source.unsplash.com/random/600x600?blossom" alt=""/>
-          <div class="description">
+          <div className="description">
             <p>blossomy blossom</p>
           </div>
         </div>
-        <div class="image">
+        <div className="image">
           <img src="https://source.unsplash.com/random/600x600?ice" alt=""/>
-          <div class="description">
+          <div className="description">
             <p>ice babyyyyy</p>
           </div>
         </div>
-        <div class="image">
+        <div className="image">
           <img src="https://source.unsplash.com/random/600x600?spring" alt=""/>
-          <div class="description">
+          <div className="description">
             <p>spring is allergic</p>
           </div>
       </div>
