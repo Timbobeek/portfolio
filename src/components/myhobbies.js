18c1e489ee25f5944e95484524d874aff1f84fb4
Tim Goloshchapov <68671029+Timbobeek@users.noreply.github.com>
2023-04-19 20:46:53 -0500

    Hobbies page (#9)
    * phone version wip
    
    * phone and pc version wip
    
    * text on hover wip
    
    * text on top of images
    
    * images with text wip
    
    * overall margins defined
    
    * mobile and pc zoomins done
    
    * bottom img corner fix
    
    * hobbies page done

--- src/components/myhobbies.js
+++ src/components/myhobbies.js
@@ -2,8 +2,74 @@ import React from 'react';
 import './myhobbies.css';
 
 const Hobbies = () => {
+
+
   return(
-    <div>My hobbies will be posted later!</div>
+    <div className='hobbiesPage'>
+      <span className='hobbiesTitle'>My Hobbies</span>
+      <div className="wrapper">
+        <div className="image" id='img1'>
+          <img src="stang.jpg" alt="driving"/>
+          <div className="description">
+            <p>Traveling by car gives me a lot of joy - some day I will be doing it in a '65 Mustang</p>
+          </div>
+        </div>
+        <div className="image" id='img2'>
+          <img src="language.jpg" alt="languages"/>
+          <div className="description">
+            <p>🤓Fluent in Russian/English, can converse in Ukrainian, learning Spanish and German</p>
+          </div>
+        </div>
+        <div className="image" id='img3'>
+          <img src="badminton.jpg" alt="badminton"/>
+          <div className="description">
+            <p>Badminton is a great alternative to my favorite sport of football ⚽ </p>
+          </div>
+        </div>
+        <div className="image" id='img4'>
+          <img src="guitar.webp" alt="guitar"/>
+          <div className="description">
+            <p>Not very good at it, but when I play, my soul rests</p>
+          </div>
+        </div>
+        <div className="image" id='img5'>
+          <img src="football.jpg" alt="football"/>
+          <div className="description">
+            <p>Football is the love of my life ❤️ </p>
+          </div>
+        </div>
+        <div className="image" id='img6'>
+          <img src="gym.jpg" alt="gym"/>
+          <div className="description">
+            <p>Bench press? Deadlift? Pull-ups? Squats? Bicep curls? Yes!</p>
+          </div>
+        </div>
+        <div className="image" id='img7'>
+          <img src="movies.png" alt="movies"/>
+          <div className="description">
+            <p>Having worked at a movie theatre for 6 years, it is impossible to not love cinema</p>
+          </div>
+        </div>
+        <div className="image" id='img8'>
+          <img src="pool.jpg" alt="billiard"/>
+          <div className="description">
+            <p>One of my most recent hobbies, I call it "chess for cool people"</p>
+          </div>
+        </div>
+        <div className="image" id='img9'>
+          <img src="pubg.jpg" alt="pubg"/>
+          <div className="description">
+            <p>I hate this game (2k+ hours)</p>
+          </div>
+        </div>
+        <div className="image" id='img10'>
+          <img src="twitch.png" alt="twitch"/>
+          <div className="description">
+            <p>Can watch a major, can watch a dude with 2 views (I tried streaming once)</p>
+          </div>
+      </div>
+    </div>
+  </div>
   );
 }
 
