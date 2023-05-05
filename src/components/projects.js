afa0acefec57c3b9e4d0f3bc4b9cfeb364d7215d
Tim Goloshchapov <tim.goloschapov@gmail.com>
2023-05-04 22:09:36 -0500

    1000px plus done

--- src/components/projects.js
+++ src/components/projects.js
@@ -7,41 +7,41 @@ const Projects = () => {
     <div className="container">
       <div className="card" id='project1'>
         <div className="card__head">
-          <div className='p1'>Project One Title</div>
-          <div className="p2">Link</div>
-          <div className="p3">Description</div>
+          <div className='p1'>Data Persistence Project</div>
+          <div className="p3">One of the toughest Backend assignments I have worked on at BloomTech: involves SQL, multi-table queries, data modeling.</div>
+          <a href='https://github.com/Timbobeek/web-sprint-challenge-adding-data-persistence' className="p2" target="_blank" rel="noreferrer">GitHub Repository</a>
         </div>
       </div>
       
       <div className="card" id='project2'>
         <div className="card__head">
-          <div className='p1'>Project Two Title</div>
-          <div className="p2">Link</div>
-          <div className="p3">Description</div>
+          <div className='p1'>A Bit Of Robotics</div>
+          <div className="p3">In this project, I have written code in Arduino (a C++ variant) to make a robot perform various actions on voice command. Fun times!</div>
+          <a href='https://github.com/Timbobeek/Arduino-Voice-Recognition-Project-2.0' className="p2" target="_blank" rel="noreferrer">GitHub Repository</a>
         </div>
       </div>
   
       <div className="card" id='project3'>
         <div className="card__head">
-          <div className='p1'>Project Three Title</div>
-          <div className="p2">Link</div>
-          <div className="p3">Description</div>
+          <div className='p1'>Mock Marketplace Website</div>
+          <div className="p3">I designed most of this Amazon/Ebay/Craigslist mock and was responsible for the home and listings pages. Also done at BloomTech, my first serious group project!</div>
+          <a href='https://github.com/african-marketplace-dec2021/frontend' className="p2" target="_blank" rel="noreferrer">GitHub Repository</a>
         </div>
       </div>
   
       <div className="card" id='project4'>
         <div className="card__head">
-          <div className='p1'>Project Four Title</div>
-          <div className="p2">Link</div>
-          <div className="p3">Description</div>
+          <div className='p1'>Mock Recipe Website</div>
+          <div className="p3">A web page made for saving recipes online, where I worked on (you guessed it!) the component responsible for saving recipes on the site.</div>
+          <a href='https://github.com/Build-Week-Secret-Family-Recipes-01/front-end' className="p2" target="_blank" rel="noreferrer">GitHub Repository</a>
         </div>
       </div>
   
       <div className="card" id='project5'>
         <div className="card__head">
-          <div className='p1'>Project Five Title</div>
-          <div className="p2">Link</div>
-          <div className="p3">Description</div>
+          <div className='p1'>A Registration Page</div>
+          <div className="p3">As a part of BloomTech bootcamp program, I made the UX/Frontend of one of the registration pages for a children's coding school.</div>
+          <a href='https://github.com/BloomTech-Labs/coder-heroes-fe/pull/234' className="p2" target="_blank" rel="noreferrer">GitHub Repository</a>
         </div>
       </div>
     </div>
@@ -49,3 +49,48 @@ const Projects = () => {
 };
 
 export default Projects;
+
+// potential projects
+
+//maybe add tech stack to each project description using logos? 
+
+
+// Image? -wip
+// A Bit Of Robotics 
+// Tech: Arduino
+// https://github.com/Timbobeek/Arduino-Voice-Recognition-Project-2.0
+// In this project, I have written code in Arduino (a C++ variant) to make a robot perform various actions on voice command. Fun times!
+
+// Image? -no
+// ?? ask papa for title
+// Tech: Typescript, Next.js 
+// https://github.com/ferrata/open-profile-page
+// ??? ask papa
+
+// Image? -wip
+// A Registration Page
+// Tech: React.js CSS 
+// https://github.com/BloomTech-Labs/coder-heroes-fe/pull/234
+// As a part of BloomTech bootcamp program, I made the UX/Frontend of one of the registration pages for a children's coding school.
+
+// Image? -wip
+// Mock Recipe Website
+// Tech: React.js CSS
+// https://github.com/Build-Week-Secret-Family-Recipes-01/front-end
+// https://recipesfr.vercel.app/
+// A web page made for saving recipes online, where I worked on (you guessed it!) the component responsible for saving recipes on the site.
+
+// Image? -wip
+// Mock Marketplace Website
+// Tech: React.js CSS
+// https://github.com/african-marketplace-dec2021/frontend
+// https://frontend-nine-flame.vercel.app/ 
+// I designed most of this Amazon/Ebay/Craigslist mock and was responsible for the home and listings pages. Also done at BloomTech, my first serious group project!
+
+// Image? -wip
+// Data Persistence Project
+// Tech: JS 
+// https://github.com/Timbobeek/web-sprint-challenge-adding-data-persistence
+// One of the toughest Backend assignments I have worked on at BloomTech: involves SQL, multi-table queries, data modeling.
+
+
