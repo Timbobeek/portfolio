583dcf7a86bd7020bb6c777ff4b7f7406c5ced56
Tim Goloshchapov <tim.goloschapov@gmail.com>
2023-11-07 23:36:16 -0600

    resume page edited

--- src/components/resume.js
+++ src/components/resume.js
@@ -18,6 +18,18 @@ const Resume = () => {
     };
   }, []);
 
+  React.useEffect(() => {
+    const interval = setAnimationInterval("contactMe", [
+      "Got a project for me? Email me at",
+      "У вас есть проект для меня? Напишите на",
+      "У вас є проект для мене? Напишіть на"
+    ]);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div className="resumeDiv">
       <span id="resumeTitle" className="resumeTitle">
@@ -26,6 +38,11 @@ const Resume = () => {
       <a href="resume.pdf" download="Timofey Goloshchapov Resume">
         <button>PRESS TO DOWNLOAD</button>
       </a>
+      <span id="contactMe" className="contactMe">
+        Resume
+      </span>
+      <span className="email">tim.goloschapov@gmail.com</span>
+
     </div>
   );
 };
